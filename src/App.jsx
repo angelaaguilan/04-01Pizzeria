e463f1c1859a986387bcd2404689193c2758b0d4
Angela Aguila <angela.aguila@gmail.com>
2024-08-02 20:48:27 -0400

    Update imagenes de public a imgs mas import

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./component/NavBar";
+import Home from "./component/Home";
+import Footer from "./component/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
